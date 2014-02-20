6ca5dae9f3076ca87ddec0c6edc615f8fcfac0c4
lebigre <remi.lebigre@gmail.com>
2014-02-20 14:47:39 +0100

    Layouts et helper titre ajoutés

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Retourner un titre basé sur la page.
+  def titre
+    base_titre = "Shorinji Kempo Club Neuilly"
+    if @titre.nil?
+      base_titre
+    else
+      "#{base_titre} | #{@titre}"
+    end
+  end
 end
