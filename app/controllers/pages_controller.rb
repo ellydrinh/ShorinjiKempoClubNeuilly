cf52340534b56153f10d81d42da0435ffaf27b25
lebigre <remi.lebigre@gmail.com>
2014-02-20 15:00:10 +0100

    Layout, helper et controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,44 @@
+#encoding: utf-8
+
+
+class PagesController < ApplicationController
+  def home
+        @titre = "Accueil"
+  end
+
+  def asso
+        @titre = "Association"
+  end
+
+  def asso1
+        @titre = "Présentation Association"
+  end
+
+  def asso2
+        @titre = "Conseil d'Administration"
+  end
+
+  def sk
+        @titre = "Shorinji Kempo"
+  end
+
+  def sk1
+      @titre = "Présentation SK" 
+  end
+
+  def sk2
+        @titre = "Historique"
+  end
+
+  def manif
+        @titre = "Manifestations"
+  end
+
+  def media
+        @titre = "Médias"
+  end
+
+  def contact
+        @titre = "Contact"
+  end
+end
