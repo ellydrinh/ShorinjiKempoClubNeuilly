0e05df0681fe66f533d26e2b40470714566a29da
lebigre <remi.lebigre@gmail.com>
2014-02-20 14:12:27 +0100

    Premier Depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShorinjiKempoClubNeuilly::Application.config.secret_key_base = '202ea3820c199eb9c32300f40f88844d91ee710a88a9aa21f4b75c2a148bd7996e3637918ecc4031157e6bb9738d27198e3f7f7773a405e9587ec166b6fbd091'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShorinjiKempoClubNeuilly::Application.config.session_store :cookie_store, key: '_ShorinjiKempoClubNeuilly_session'
