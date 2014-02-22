fed37928721a3bddf631d8ae2a9ca739527b7739
Benoit JOSEPH <royallys@gmail.com>
2014-02-22 15:22:38 +0100

    fix mail

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -1,7 +1,8 @@
 class ContactsMailer < ActionMailer::Base
 	default :from => "testmail@gmail.com"
 
-	def send_email
+	def send_email(message)
+		@message = message
 		mail :to => "greumph@hotmail.fr", :subject => "Nouveau message SK"
 	end
 end
