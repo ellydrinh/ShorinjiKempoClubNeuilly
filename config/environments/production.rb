4c3d4ec4e1e3204206e681bbd561f557717d9d90
Benoit JOSEPH <royallys@gmail.com>
2014-03-12 23:20:44 +0100

    jean nemarre

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,11 +77,6 @@ ShorinjiKempoClubNeuilly::Application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
-  config.middleware.use ExceptionNotifier,
-      :email_prefix => "[Shorinji Kempo SK Neuilly] ",
-      :sender_address => %{"notifier" <exceptions@benoitjoseph.com>},
-      :exception_recipients => %w{dev.benoit.joseph@gmail.com}
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
