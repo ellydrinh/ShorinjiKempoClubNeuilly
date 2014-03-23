8ea9f1fb0282f952207675a7d9444bdab3c17fa3
Benoit JOSEPH <royallys@gmail.com>
2014-03-23 21:15:11 +0100

    use git read-only url and change gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "skneuilly"
-set :repository, "git@github.com:ellydrinh/ShorinjiKempoClubNeuilly.git"
+set :repository, "git://github.com/ellydrinh/ShorinjiKempoClubNeuilly.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
